fdac741b668a7f078b81f201f62bc221dbe091d8
Johnny Horvi <johnny@horvi.no>
2020-06-25 08:43:49 +0200

    test

--- Casks/agent.rb
+++ Casks/agent.rb
@@ -2,7 +2,7 @@ cask 'agent' do
   version '2020-06-24-ea01db4'
   sha256 '872fa6a4e788c3c2cfd639b04cfaf282e96745921db8fd62cfe585392eb00dea'
 
-  url "https://github.com/nais/device/releases/download/2020-06-24-ea01db4/naisdevice-#{version}.pkg"
+  url "https://github.com/nais/device/releases/download/#{version}/naisdevice-#{version}.pkg"
   name 'naisdevice agent'
   homepage 'https://doc.nais.io/device'
 
